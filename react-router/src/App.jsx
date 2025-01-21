b76546e93c292191a7b4a420fc6b108aa54d014c
AzeemRafique10 <urbancrime19@gmail.com>
2025-01-21 17:19:52 +0500

    react routing app

--- react-router/src/App.jsx
+++ react-router/src/App.jsx
@@ -1,11 +1,20 @@
-import Header from "./components/Header/Header"
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
+import Home from './components/Home/Home'
 
-const App = () => {
+function App() {
+  const [count, setCount] = useState(0)
+
   return (
     <>
-      <Header />
+     
+      
     </>
-  );
-};
+  )
+}
 
-export default App;
+export default App
