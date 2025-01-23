fb999bcd4bd6ba3b2528fdbab4256d0db5017b14
AzeemRafique10 <urbancrime19@gmail.com>
2025-01-23 12:45:30 +0500

    context api project

---
+++ context-project/src/App.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <>
+      <h1 className="bg-slate-200 text-5xl text-center ">
+        Context API Project
+      </h1>
+    </>
+  );
+};
+
+export default App;
